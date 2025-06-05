2ed2805e90004490da505dca3b13174f5c12eaab
MrChub467 <chubster467@gmail.com>
2025-06-05 11:26:15 -0500

    add submit button

--- weather.js
+++ weather.js
@@ -2,15 +2,29 @@
   const URL =
     "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
   const API_KEY = "7W5W9KNW3ZF8EYXKZHNH3S29L";
-  const PLACE = "greenbay";
+  const cityInput = document.getElementById("city");
+  const submitBtn = document.querySelector(".city-input button");
+  submitBtn.addEventListener("click", () => {
+    if (cityInput.value) {
+      let place = cityInput.value.toLowerCase().replace(" ", "");
+      cityInput.value = "";
+      getWeatherData(place, API_KEY);
+    }
+  });
 
   async function getWeatherData(place, key) {
-    const response = await fetch(URL + `${place}/?key=${key}`, {
-      mode: "cors",
-    });
-    const json = await response.json();
-    console.log(json);
+    try {
+      const response = await fetch(URL + `${place}/?key=${key}`, {
+        mode: "cors",
+      });
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.log(error);
+    }
   }
+
+  function displayWeatherData(data) {}
 
   // getWeatherData(PLACE, API_KEY);
 })();
